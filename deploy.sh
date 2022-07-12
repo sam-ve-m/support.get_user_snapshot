9bf52911df079af25921df503f3258cffa838065
Vinicius Reis <vro@lionx.com.br>
2022-07-12 16:19:41 -0300

    create specs uat, update specs dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
-fission env create --spec --name post-user-ticket-env --image nexus.sigame.com.br/python-env-3.8:0.0.5 --builder nexus.sigame.com.br/python-builder-3.8:0.0.2
-fission fn create --spec --name post-user-ticket-fn --env post-user-ticket-env --src "./func/*" --entrypoint main.get_user_snapshot --executortype newdeploy --maxscale 1
-fission route create --spec --method POST --url /get_user_snapshot --function get-user-snapshot-fn
+fission env create --spec --name get-user-snapshot-env --image nexus.sigame.com.br/python-env-3.8:0.0.5 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
+fission fn create --spec --name get-user-snapshot-fn --env get-user-snapshot-env --src "./func/*" --entrypoint main.get_user_snapshot
+fission route create --spec --name get-user-snapshot-rt --method GET --url /get_user_snapshot --function get-user-snapshot-fn
