a0e2bf4f29928a9d37ccb3c4a21eb62b3465a635
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-07-15 18:36:50 -0300

    adjusting specs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
 fission env create --spec --name get-user-snapshot-env --image nexus.sigame.com.br/fission-async:0.1.6 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
-fission fn create --spec --name get-user-snapshot-fn --env get-user-snapshot-env --src "./func/*" --entrypoint main.get_user_snapshot
-fission route create --spec --name get-user-snapshot-rt --method GET --url /get_user_snapshot --function get-user-snapshot-fn
+fission fn create --spec --name get-user-snapshot-fn --env get-user-snapshot-env --src "./func/*" --entrypoint main.get_user_snapshot --executortype newdeploy --maxscale 1
+fission route create --spec --name get-user-snapshot-rt --method GET --url /support/get_user_snapshot--function get-user-snapshot-fn
